8c8e710928ac7fb65c17e3aa61caf411f539d65a
codestack82 <codestack82@gmail.com>
2021-08-29 15:43:19 -0500

    add config & title

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+# Streamlit api ref - https://docs.streamlit.io/en/stable/api.html
+
 # Set initial app configs
 st.set_page_config(
    page_title="Data Journey App",
