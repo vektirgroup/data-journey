b71538cd03f0d9e19e19daf1d5d0e7a7cdfc11a4
codestack82 <codestack82@gmail.com>
2021-08-29 15:33:06 -0500

    update app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+# Set initial app configs
 st.set_page_config(
    page_title="Data Journey App",
    page_icon="🧊",
    layout="wide",
    initial_sidebar_state="expanded",
 )
+
+df1 = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20))
+)
+
+my_table = st.table(df1)
